bebcdf6b50b78bd247c65565b96524b13795fdce
joniel santiago <santij67@gmatc.matc.edu>
2025-09-17 17:39:53 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import {check, validationResult } from 'express-validator';
 
 // Initial express application
 const app = express();
@@ -8,10 +9,37 @@ const app = express();
 // Connect to the database
 connectDatabase();
 
-// API endpoints
+
+app.use(express.json({extend: false}));
+// API Endpoints
+/**
+ * @route GET /
+ * @desc  Test endpoint
+ */
+
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route
+ * @desc
+ */
+app.post('/api/users', [
+    check ('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+    } else {
+    // Later we will save the user to the database here
+    return res.send(req.body);
+}
+console.log(req.body);
+res.send(req.body);
+});
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
